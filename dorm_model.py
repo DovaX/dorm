4dba7075cb6e2a5936ba2dedab9dd2f4084cbaed
Dominik <vach.dominik@gmail.com>
2019-08-15 21:10:04 +0200

    corr

--- dorm_model.py
+++ dorm_model.py
@@ -6,7 +6,7 @@ table1 = Table(db1,"test",["test1","test2","test3","test4"],["int","int","int","
 table1.create()
 rows=[[1,2,3,4],[5,4,7,9]]
 table1.insert(rows)
-list1=table1.select("SELECT test1 FROM testovaci")
+list1=table1.select("SELECT test1 FROM test")
 print(list1)
 
 list2=table1.select_all()
