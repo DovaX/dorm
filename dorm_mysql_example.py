2c9f032922355ab5c1a98793d0077579b5b65618
Dominik Vach <vach.dominik@gmail.com>
2020-04-03 13:30:35 +0200

    example not create and drop table

--- dorm_mysql_example.py
+++ dorm_mysql_example.py
@@ -3,7 +3,7 @@ import pandas as pd
 db1=dm.Mysqldb()
 
 table1=dm.MysqlTable(db1,"test",["id","test2","test3"],["int","int","int"])
-table1.create()
+#table1.create()
 
 rows=[[2,2]]
 
@@ -14,6 +14,6 @@ table1.insert_from_df(df)
 rows=table1.select_all()
 print(rows)
 
-table1.drop()
+#table1.drop()
 
 db1.close_connection()
