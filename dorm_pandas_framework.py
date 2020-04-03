49391ecf9a3e1d679ee84ad062d0427b95458ff7
Dominik Vach <vach.dominik@gmail.com>
2020-04-03 14:01:06 +0200

    dorm_pandas_framework edit

--- dorm_pandas_framework.py
+++ dorm_pandas_framework.py
@@ -1,21 +1,19 @@
 import dorm as dm
 import pandas as pd
 
-def initialize_dataframes_from_tables(db1,list_of_table_names):
-    dorm_tables={}
-    dfs={}
+def init_dorm_tables(db1,list_of_table_names,dorm_tables={}):
     for table_name in list_of_table_names:
-        dorm_tables[table_name]=dm.Table.init_all_columns(db1,table_name)
+        dorm_tables[table_name]=dm.Table.init_all_columns(db1,table_name)  
+    return(dorm_tables)
+
+def init_dataframes_from_tables(db1,dorm_tables,list_of_table_names,dfs={}):  
+    for table_name in list_of_table_names:
         dfs[table_name]=dorm_tables[table_name].select_to_df()
-    return(dorm_tables,dfs)
+    return(dfs)
 
-
-def initialize_dicts_between_dataframes(dfs,dictionary_definitions):
+def init_dicts_between_dataframes(dfs,dictionary_definitions,dicts={}):
     #input list of lists of 4 strings: df_name,column1,column2,dict_name
     dicts={}
     for definition in dictionary_definitions:
         dicts[definition[3]]=dm.df_to_dict(dfs[definition[0]],definition[1],definition[2])  
     return(dicts)
-
-
-#demand_unloading_address_dict=dm.df_to_dict(dfs['demands'],"id","unloading_address_id") #used for matching model
